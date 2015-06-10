e4230b30b8c6a65c05c357f33215b673a8f84f02
erik van widenfelt <ew2789@gmail.com>
2015-06-10 07:46:59 +0200

    rename management command, imprt load_csv method

--- getresults/management/commands/load_definitions.py
+++ getresults/management/commands/load_definitions.py
@@ -5,11 +5,12 @@ from unipath import Path
 
 from django.core.management.base import BaseCommand, CommandError
 
+from getresults_csv.utils import load_csv_file
 from getresults.utils import load_panels_from_csv, load_utestids_from_csv, load_panel_items_from_csv
 
 
 class Command(BaseCommand):
-    help = 'Import data from a folder containing panels.csv, utestids.csv, panel_items.csv and csv_headers.csv'
+    help = 'Load data from a folder containing panels.csv, utestids.csv, panel_items.csv'
 
     def add_arguments(self, parser):
         parser.add_argument('path', nargs='+', type=str)
@@ -22,15 +23,7 @@ class Command(BaseCommand):
                 files.append(('panels.csv', load_panels_from_csv))
                 files.append(('utestids.csv', load_utestids_from_csv))
                 files.append(('panel_items.csv', load_panel_items_from_csv))
-                sys.stdout.write('Importing from {}...\n'.format(str(path)))
-                for filename, load_func in files:
-                    try:
-                        sys.stdout.write('    {}'.format(filename))
-                        load_func(os.path.join(path, filename))
-                        sys.stdout.write('. OK\n')
-                    except (FileNotFoundError, ) as e:
-                        sys.stdout.write('. ERROR {}\n'.format(str(e)))
-                sys.stdout.write('Done.\n'.format(str(path)))
+                load_csv_file(path, files)
             except (FileNotFoundError, ) as e:
                 sys.stdout.write('\n')
                 raise CommandError(e)
