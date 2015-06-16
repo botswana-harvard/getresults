241fd16e33bac262ca067e3a0b62b92bf2a75bb0
erik van widenfelt <ew2789@gmail.com>
2015-06-16 16:10:50 +0200

    add dispatcher logic for interfaces

---
+++ getresults/models/sender.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+from edc_base.model.models import BaseUuidModel, HistoricalRecords
+
+
+class Sender(BaseUuidModel):
+
+    name = models.CharField(
+        max_length=25,
+        unique=True
+    )
+
+    description = models.CharField(
+        max_length=100,
+        null=True
+    )
+
+    history = HistoricalRecords()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        app_label = 'getresults'
+        db_table = 'getresults_sender'
+        ordering = ('name', )
